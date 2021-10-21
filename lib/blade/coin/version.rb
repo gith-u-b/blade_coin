e13c6e12173cf6a191f3a0895e14b5bcc9d6d874
sai <rubyer1993@gmail.com>
2021-10-21 16:43:29 +0800

    update

---
+++ lib/blade/coin/version.rb
@@ -0,0 +1,5 @@
+module Blade
+  module Coin
+    VERSION = "0.1.0"
+  end
+end
--- lib/blade_coin.rb
+++
@@ -1,6 +0,0 @@
-require "blade_coin/version"
-
-module BladeCoin
-  class Error < StandardError; end
-  # Your code goes here...
-end
